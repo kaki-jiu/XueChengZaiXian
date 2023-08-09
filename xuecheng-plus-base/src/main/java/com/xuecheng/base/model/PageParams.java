d50ac693d235fba6353cbed7514a75d455720a1d
kaki-jiu <lizhenhui29393@gmail.com>
2023-08-09 21:04:35 +0800

    feature: content-api实现, 实体类追加swagger注解

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -1,34 +1,31 @@
 package com.xuecheng.base.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
-import lombok.extern.java.Log;
 
 /**
- * @description 分页查询通用参数
  * @author Mr.M
- * @date 2022/9/6 14:02
  * @version 1.0
+ * @description 分页查询分页参数
+ * @date 2023/2/11 15:33
  */
 @Data
 @ToString
 public class PageParams {
 
     //当前页码
+    @ApiModelProperty("页码")
     private Long pageNo = 1L;
+    //每页显示记录数
+    @ApiModelProperty("每页记录数")
+    private Long pageSize = 30L;
 
-    //每页记录数默认值
-    private Long pageSize =10L;
-
-    public PageParams(){
-
+    public PageParams() {
     }
 
-    public PageParams(long pageNo,long pageSize){
+    public PageParams(Long pageNo, Long pageSize) {
         this.pageNo = pageNo;
         this.pageSize = pageSize;
     }
-
-
-
 }
--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -2,26 +2,43 @@ package com.xuecheng.content.api;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @description 课程信息编辑接口
  * @author Mr.M
- * @date 2022/9/6 11:29
  * @version 1.0
+ * @description TODO
+ * @date 2023/2/11 15:44
  */
+@Api(value = "课程信息管理接口",tags = "课程信息管理接口")
 @RestController
 public class CourseBaseInfoController {
 
-    @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams){
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
 
-        return null;
+    @ApiOperation("课程查询接口")
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required=false) QueryCourseParamsDto queryCourseParamsDto) {
+
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+
+        return courseBasePageResult;
 
     }
 
